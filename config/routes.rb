33145ff12ce413783e2dd970d42a53f1c84a2a8e
Elise Wetherholt <ewether@gmail.com>
2020-07-27 22:11:20 -0400

    added views & sass files, base html/styling for index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
+  root to: 'pages#index'
 
-  get 'about', to: 'pages#about'
-  get 'portfolio', to: 'pages#portfolio'
-  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about', as: :about
+  get 'portfolio', to: 'pages#portfolio', as: :portfolio
+  get 'contact', to: 'pages#contact', as: :contact
 end
